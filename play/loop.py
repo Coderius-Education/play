ecf58c5ef543f43a93219acff7a15f086ef1ce4f
Koen <98043234+koen1711@users.noreply.github.com>
2025-12-03 22:00:21 +0100

    chore: add error catching for _loop tasks (#17)
    Signed-off-by: Marten Postma <postmam@corderius.nl>
    Co-authored-by: Marten Postma <postmam@corderius.nl>

--- play/loop.py
+++ play/loop.py
@@ -1,6 +1,25 @@
 """This module is used to create a global event loop for the application."""
 
 import asyncio
+import traceback
+import pygame
+from .io.logging import play_logger
+
+
+def _handle_exception(loop, context):
+    exception = context.get("exception")
+    task = context.get("future")
+    task_name = task.get_name() if task else "unknown"
+
+    if exception:
+        tb_lines = traceback.format_exception(type(exception), exception, exception.__traceback__)
+        tb_str = ''.join(tb_lines)
+        play_logger.critical(f"Unhandled exception in task '{task_name}':\n{tb_str}")
+    else:
+        play_logger.critical(context.get("message", "Unhandled exception in async task"))
+
+    loop.stop()
+
 import sys
 
 # Python 3.14+ changed asyncio.get_event_loop() behavior
@@ -13,3 +32,4 @@ else:
     loop = asyncio.get_event_loop()
 
 loop.set_debug(False)
+loop.set_exception_handler(_handle_exception)
