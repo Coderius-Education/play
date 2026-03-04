00b9207c05760dcb725a76d74446e36496ec599c
MartenPostma <postmam@corderius.nl>
2026-03-04 09:36:47 +0100

    chore: fixed warning

--- play/core/game_loop_wrapper.py
+++ play/core/game_loop_wrapper.py
@@ -3,6 +3,7 @@ This module contains a decorator that listens to exceptions in the game loop.
 """
 
 import asyncio
+import functools
 
 from ..io.logging import play_logger
 from ..loop import get_loop as _get_loop
@@ -13,16 +14,27 @@ def listen_to_failure():
     """A decorator that listens to exceptions in the game loop."""
 
     def decorate(f):
-        async def applicator(*args, **kwargs):
-            try:
-                result = f(*args, **kwargs)
-                if asyncio.iscoroutine(result):
-                    return await result
-                return result
-            except Exception as e:
-                _get_loop().stop()
-                play_logger.critical("Error in %s: %s", f.__name__, e)
-                raise e
+        if asyncio.iscoroutinefunction(f):
+
+            @functools.wraps(f)
+            async def applicator(*args, **kwargs):
+                try:
+                    return await f(*args, **kwargs)
+                except Exception as e:
+                    _get_loop().stop()
+                    play_logger.critical("Error in %s: %s", f.__name__, e)
+                    raise e
+
+        else:
+
+            @functools.wraps(f)
+            async def applicator(*args, **kwargs):
+                try:
+                    return f(*args, **kwargs)
+                except Exception as e:
+                    _get_loop().stop()
+                    play_logger.critical("Error in %s: %s", f.__name__, e)
+                    raise e
 
         return applicator
 
